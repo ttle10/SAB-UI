2ef9fe670592de831422c02ec0562c0e7ca6478b
ttle10 <thete.le@stm.info>
2026-04-29 21:31:10 -0400

    Fix Profile Data Config

--- SystemeAideBasculement_v1/SystemeAideBasculement/Models/SabProfileRow.cs
+++ SystemeAideBasculement_v1/SystemeAideBasculement/Models/SabProfileRow.cs
@@ -2,8 +2,9 @@
 {
     public class SabProfileRow
     {
-        public int DisplayIndex { get; set; } = 0;
+        public int Index { get; set; } = 0;
         public string Profile { get; set; } = "";
+        public string DisplayName { get; set; } = "";
 
         public Endpoint CCP { get; set; } = new();
         public Endpoint CCR { get; set; } = new();
