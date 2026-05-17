d119fe9c25385c35e0ac373d5a2915015b6102eb
ttle10 <thete.le@stm.info>
2026-05-16 21:59:52 -0400

    Finalyse AideMemoire UI and Functions

---
+++ SystemeAideBasculement_v1/SystemeAideBasculement/wwwroot/js/app.js
@@ -0,0 +1,7 @@
+﻿window.autoResizeTextarea = (el) => {
+    if (!el) return;
+
+    el.style.height = "auto";       // reset
+    el.style.overflow = "hidden";   // pas de scrollbar
+    el.style.height = el.scrollHeight + "px"; // ajuste hauteur
+};
