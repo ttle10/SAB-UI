fb88f5170dd7e5a80c470de115aacdc709e7a991
ttle10 <thete.le@stm.info>
2026-05-23 17:15:36 -0400

    Replace AidememoireModel with AidememoireVM in components

---
+++ SystemeAideBasculement_v1/SystemeAideBasculement/Models/AideMemoireVm.cs
@@ -0,0 +1,32 @@
+﻿namespace SystemeAideBasculement.Models
+{
+    public class AideMemoireVm
+    {
+        public AideMemoireVm()
+        { 
+        }
+
+        public AideMemoireVm(AideMemoireVm vm)
+        {
+            Id = vm.Id;
+            Step = vm.Step;
+            Initials = vm.Initials;
+            IsCompleted = vm.IsCompleted;
+        }
+        public int Id { get; set; }
+        public string? Step { get; set; } = string.Empty;
+        public string? Initials { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public static class AideMemoireMapping
+    {
+        public static AideMemoireVm ToVm(this AideMemoireModel e) => new()
+        {
+            Id = e.Id,
+            Step = e.Step,
+            Initials = e.Initials,
+            IsCompleted = e.IsCompleted
+        };
+    }
+}
