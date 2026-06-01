39c9da46e601133b904cc9e2a786cfe6e40aedb3
ttle10 <thete.le@stm.info>
2026-06-01 11:43:05 -0400

    Fix : HubConnection.StartAsync() is being called when the connection is already starting/connected, so SignalR throws that InvalidOperationException

--- SystemeAideBasculement_v1/SystemeAideBasculement/Services/NotificationService.cs
+++ SystemeAideBasculement_v1/SystemeAideBasculement/Services/NotificationService.cs
@@ -6,45 +6,60 @@ namespace SystemeAideBasculement.Services
 {
     public class NotificationService : IAsyncDisposable
     {
-        private HubConnection? _connection;
+        private HubConnection? _hubConnection;
 
         public event Action? OnStateUpdated;
 
         public event Action<Guid>? OnReminderChanged;
 
+        private bool _started;
+
         static public string ReminderChangedMethod => "ReminderChanged";
 
         public async Task StartAsync(NavigationManager nav)
         {
-            if (_connection is not null)
-                return;
-
-            _connection = new HubConnectionBuilder()
-               .WithUrl(nav.ToAbsoluteUri("/notifications"))
-               .WithAutomaticReconnect()
-               .Build();
-
-            _connection.On<SabDataNotification>(
-                "ProfileUpdated",
-                _ =>
-                {
-                    OnStateUpdated?.Invoke();
-                });
-
-            _connection.On<Guid>(ReminderChangedMethod, (initiator) =>
+            // If we've already started once in this circuit, do nothing
+            if (_started && _hubConnection is not null &&
+                _hubConnection.State != HubConnectionState.Disconnected)
             {
-                OnReminderChanged?.Invoke(initiator);
-            });
+                return;
+            }
 
-            await _connection.StartAsync();
+            if (_hubConnection is null)
+            {
+                _hubConnection = new HubConnectionBuilder()
+                               .WithUrl(nav.ToAbsoluteUri("/notifications"))
+                               .WithAutomaticReconnect()
+                               .Build();
+
+                _hubConnection.On<SabDataNotification>(
+                    "ProfileUpdated",
+                    _ =>
+                    {
+                        OnStateUpdated?.Invoke();
+                    });
+
+                _hubConnection.On<Guid>(ReminderChangedMethod, (initiator) =>
+                {
+                    OnReminderChanged?.Invoke(initiator);
+                });
+            }
+
+            // Only start if currently disconnected
+            if (_hubConnection.State == HubConnectionState.Disconnected)
+            {
+                await _hubConnection.StartAsync();
+            }
+
+            _started = true;
         }
 
         public async ValueTask DisposeAsync()
         {
 
-            if (_connection != null)
+            if (_hubConnection != null)
             {
-                await _connection.DisposeAsync();
+                await _hubConnection.DisposeAsync();
             }
         }
     }
